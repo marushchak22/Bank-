21ec7ade8881fd77c8de9fdc810b41ce2518e1e7
Nazar <marusaknazar0@gmail.com>
2024-11-06 22:52:16 +0200

    Bank first version

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Вітаю ви потрапили в НазарБанк!");
+
+            User user = new User(100);
+
+            Console.WriteLine("Якщо ви хочете зняти кошти натисніть 1 якщо покласти то 2 якщо переглянути баланс 3");
+            int operation = int.Parse(Console.ReadLine());
+            switch (operation)
+            {
+                case 1:
+                    Console.WriteLine("скільки грошей ви хочете зняти з балансу?");
+                    user.Withbraw(1000);
+                    break;
+                case 2:
+                    Console.WriteLine("скільки грошей ви хочете покласти на баланс?");
+                    user.Recive(1000);
+                    break;
+                case 3:
+                    user.ShowBalance(1);
+                    break;
+
+            }Console.ReadKey();
+        }
+        
+
+
+       
+    }
+}
+
---
+++ User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class User : IUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public double Balance {  get; set; }
+
+
+
+        public User(double Balance)
+        {
+         
+            this.Balance = Balance;
+       }
+
+        public double ShowBalance(int Id)
+        {
+            return Balance;
+            Console.WriteLine($"Ваш баланс {Balance}");
+        }
+
+
+        public double Withbraw(double SizeOfWithdrow)
+        {
+            double withdrow = Balance - SizeOfWithdrow;
+            Balance = withdrow;
+            return withdrow;
+
+            Console.WriteLine($"Зняття успішне! Ваш баланс {Balance} ");
+        }
+
+        public double Recive(double SizeOfRecive)
+        {
+            double recive = Balance + SizeOfRecive;
+            Balance = recive;
+            Console.WriteLine($"Депозит успішний! Ваш баланс {this.Balance} ");
+            return recive;
+        }
+    }
+}
